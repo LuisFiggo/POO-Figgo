a33444fc6fa598a3532a3b71938f7573f3610399
LuisFiggo <luis.quispe69@upeu.edu.pe>
2026-09-04 16:38:34 -0500

    Avance proyecto P1

---
+++ SysVentas/src/main/java/pe/edu/upeu/sysventas/model/Cliente.java
@@ -0,0 +1,15 @@
+package pe.edu.upeu.sysventas.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import pe.edu.upeu.sysventas.enums.TipoDocumento;
+
+@Getter
+@Setter
+public class Cliente {
+    String dniruc;
+    String nombres;
+    String redLegal;
+    String direccion;
+    TipoDocumento tipoDocumento;
+}
