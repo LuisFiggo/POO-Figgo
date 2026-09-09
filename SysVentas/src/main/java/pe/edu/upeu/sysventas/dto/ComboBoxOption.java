5ad0464d7a5a0fcd5889b0a1a1e1d14d8a510ca1
AndersonDiazDev <diazccamathomas@gmail.com>
2026-09-09 12:16:28 -0500

    Avance 2

---
+++ SysVentas/src/main/java/pe/edu/upeu/sysventas/dto/ComboBoxOption.java
@@ -0,0 +1,17 @@
+package pe.edu.upeu.sysventas.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ComboBoxOption {
+    String key;
+    String value;
+    @Override
+    public String toString() {
+        return value;
+    }
+}
